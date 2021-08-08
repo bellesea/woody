e3124f0d64d35ff4a41afbd0493bc7445b605ae0
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-08-07 22:50:11 -0500

    seo stuff

---
+++ components/Head.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function HeadObject({children}) {
+    const title = "Woody Keppel";
+    const description = "actor • comedian • songwriter";
+    const keywords = "woody, keppel, actor, comedian, songwriter, vermont";
+    const author = "Woody Keppel";
+    return (
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords} />
+            <meta name="author" content={author} />
+            {children}
+        </Head>
+    )
+}
